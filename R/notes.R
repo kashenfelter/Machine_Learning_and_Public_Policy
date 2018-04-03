87b7041cdda392bdd153a6a5fa0a6716c5258209
cenuno <nuno.e.cristian@gmail.com>
2018-04-02 20:25:45 -0500

    Imported ISLR package to follow notes in ISL book.

--- R/notes.R
+++ R/notes.R
@@ -4,4 +4,26 @@
 # Date:     April 2, 2018
 #
 
+# load necessary packages
+# install.packages( pkgs = "ISLR" )
+library( ISLR )
+
+# In particular, we wish to understand the association between 
+# an employee’s age and education, 
+# as well as the calendar year, on his wage
+plot(
+  x = Wage$age
+  , y = Wage$wage
+  , pch = 19
+  , col = "#CCCCCC"
+  , main = "Wage v. Age"
+)
+lines( x = lowess(
+  x = Wage$age
+  , y = Wage$wage
+)
+, col = "darkgoldenrod2"
+, lwd = 4
+)
+
 # end of script #
